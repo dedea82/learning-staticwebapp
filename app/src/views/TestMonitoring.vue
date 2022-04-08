f3b97a2de134728c7a70fa12ef98a4367cfd90ad
dedea82 <fumagalli.andrea82@outlook.com>
2022-04-08 11:49:31 +0200

    Add local tracking with Application Insights

--- app/src/views/TestMonitoring.vue
+++ app/src/views/TestMonitoring.vue
@@ -7,11 +7,14 @@
     <div><button v-on:click="fatal()">Unhandled js-exception (Vue)</button> </div>
     <div><button onclick="call_fake_method('pippo')">Unhandled js-exception (javascript)</button> </div>
     <div><button onclick="console.log( [3, 5, 7, 15].find(e => e === 7) );">Exception only in IE 10</button> </div>
+    <div><button v-on:click="track()">Local tracking (javascript)</button> </div>
     <div><br />{{ message }}</div>
   </div>
 </template>
 
 <script>
+import appInsights from "@/vue.appinsights.js";
+
 export default {
   name: "TestMonitoring",
   data() {
@@ -30,6 +33,17 @@ export default {
       this.message = null;
       var not_real_undefined = undefined;
       this.message = "trying to reading undefined: " + not_real_undefined.text; // unhandled exception;
+    },
+    track() {
+      appInsights.trackEvent({ name: "LocalTracking" });
+      appInsights.trackEvent({
+        name: "LocalTrackingWithProperty",
+        properties: { myValue: "BOH" },
+      });
+      appInsights.trackMetric({
+        name: "LocalMetric",
+        average: Math.random() * 10,
+      });
     },
   },
 };
